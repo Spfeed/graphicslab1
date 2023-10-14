574408fd23d52e5541e7e4e0c743bf5778885977
Spfeed <subbart2266@gmail.com>
2023-10-14 12:21:47 +0400

    испралена ФАТАЛЬНАЯ ОШИБКА v8.1 pytest исправляется

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,4 +1,12 @@
 import unittest
+import sys
+import os
+
+# Получаем путь к текущему каталогу скрипта
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Добавляем путь к родительскому каталогу
+sys.path.append(os.path.join(current_dir, '..'))
+
 
 
 class MyTestCase(unittest.TestCase):
