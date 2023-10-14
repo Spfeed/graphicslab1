a84341070457d6ae77015315b43c675ee7ebc619
Spfeed <subbart2266@gmail.com>
2023-10-14 10:52:50 +0400

    ci

--- tests/Test1.py
+++ tests/Test1.py
@@ -5,6 +5,6 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
 
-
+#бесполезный комментарий для коммита
 if __name__ == '__main__':
     unittest.main()
